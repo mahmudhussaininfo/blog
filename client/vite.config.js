ea96a9d8cac21e52ea244cac4311108eda119190
mahmud <mahmudhussaininfo@gmail.com>
2024-12-08 18:00:54 +0600

    vercel change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  base: "./", // Use relative paths
+  build: {
+    outDir: "dist", // Ensure the output folder is correct
   },
 });

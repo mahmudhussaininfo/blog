6ae039a21fe81a62024005fe3c080215fbd880dc
mahmud <mahmudhussaininfo@gmail.com>
2024-12-08 16:44:24 +0600

    update error

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
   const [blog, setBlog] = useState([]);
   useEffect(() => {
     const getData = async () => {
-      const response = await axios.get("http://localhost:5050/allBlogs");
+      const response = await axios.get("http://localhost:5050/api/allBlogs");
       console.log(response.data.blogs);
       setBlog(response.data.blogs);
     };

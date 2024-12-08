55a3473b6c7fad53b0e348ef8384a8ee2d2e5f36
mahmud <mahmudhussaininfo@gmail.com>
2024-12-08 12:44:44 +0600

    upload again

---
+++ config/config.js
@@ -0,0 +1,23 @@
+export const PORT = 5050;
+export const DATABASE =
+  "mongodb+srv://mahmudhussaininfo:MSUVITS63JIaSUxv@cluster0.i90ld.mongodb.net/blog";
+export const JWT_KEY = "3dvEkWVdMcWW8nfPnNaCXA";
+export const JWT_EXPIRE_TIME = 30 * 24 * 60 * 60;
+
+export const EMAIL_HOST = "smtp.gmail.com";
+export const EMAIL_PORT = 587;
+export const EMAIL_SECURITY = false;
+export const EMAIL_USER = "mahmudhussain504@gmail.com";
+export const EMAIL_PASS = "xqlimwnafqcivove";
+export const EMAIL_UN_AUTH = false;
+
+export const WEB_CACHE = false;
+export const MAX_JSON_SIZE = "10MB";
+export const URL_ENCODE = true;
+
+export const REQUEST_TIME = 20 * 60 * 1000;
+export const REQUEST_NUMBER = 2000;
+
+export const CLOUDINARY_CLOUD_NAME = "dcli0sqrt";
+export const CLOUDINARY_API_KEY = "487918795342748";
+export const CLOUDINARY_API_SECRET = "Vcq80aIBDzuXBbzJApxkOp6g8BY";
